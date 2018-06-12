124ac4698775374a627f83bf155bda39854f1331
jihonlee <jihonlee@naver.com>
2018-06-12 16:19:14 +0900

    Add notice list activity and adapter

---
+++ app/src/main/java/kr/hs/dgsw/flow/activity/NoticeActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.flow.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kr.hs.dgsw.flow.R;
+
+public class NoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice);
+    }
+}
---
+++ app/src/main/java/kr/hs/dgsw/flow/adapter/NoticeListAdapter.java
@@ -0,0 +1,22 @@
+package kr.hs.dgsw.flow.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class NoticeListAdapter extends RecyclerView.Adapter {
+    
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
